7053bb31c61537e14319393577d40464f4e77752
ITWeiss <gottfried98@bk.ru>
2024-11-03 16:27:00 +0700

    GreetingProgram.

---
+++ src/GreetingProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GreetingProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрос имени
+        System.out.println("Введите ваше имя:");
+        String name = scanner.nextLine();
+
+        // Запрос возраста
+        System.out.println("Введите ваш возраст:");
+        int age = scanner.nextInt();
+
+        // Вывод сообщения
+        System.out.println("Ваше имя - " + name + ", ваш возраст - " + age + " лет.");
+
+        scanner.close();
+
+    }
+}
